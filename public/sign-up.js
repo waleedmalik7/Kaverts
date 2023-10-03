816eec3b6051171831de3d40e4ec6a3d8ca66c76
waleedmalik7 <waleedmalik027@gmail.com>
2023-10-02 21:16:21 -0400

    fixed password checker issue

--- public/sign-up.js
+++ public/sign-up.js
@@ -4,19 +4,38 @@ const email_regex = /^[\w\.-]+@[\w]+\.[a-zA-Z]{2,}$/;
 const password_digit = /(?=.*\d)/;
 const password_cl = /(?=.*[A-Z])/;
 const password_regex = /^[\w!@#$%^&*]{8,15}$/;
+const submitValid = true;
 
 //query selectors
 const form = document.querySelector(".sign-up-form");
-const name = document.querySelector("#full-name");
-const username = document.querySelector("#username");
-const password = document.querySelector("#password");
-const conf_password = document.querySelector("#confirm-password");
 const checkbox = document.querySelector(".checkbox");
+const password = document.querySelector("#password");
+const information = [
+    document.querySelector("#full-name"),
+    document.querySelector("#username"),
+    document.querySelector("#password"),
+    document.querySelector("#confirm-password")
+];
 
 //Non database checks: 
 form.addEventListener('submit', (e)=>{
     e.preventDefault(); //stops refresh
+    if(checkbox.checked && submittable()){
+        alert('Form submitted successfully');
+        form.submit();
+    }else{
+        alert('Form is invalid');
+    }
 });
+
+function submittable(){
+    for(let i = 0; i < information.length; i++){
+        if(information[i].getAttribute('class') == 'fail'){
+            return false;
+        }
+    }
+    return true;
+};
 
 function validateAndSetClass(element, regex){
     const val = element.value;
@@ -43,13 +62,12 @@ form.addEventListener('keyup', e => {
         const isCorrectLength = password_regex.test(val);
    
         if (hasDigit && hasCapital && isCorrectLength) { 
-            
             element.setAttribute('class', 'success');
         } else {
             element.setAttribute('class', 'fail');
         }
     }else if(names === "confirm-password"){
-        if(conf_password.value === password.value){
+        if(val === password.value){
             element.setAttribute('class','success');
         }else{
             element.setAttribute('class','fail');
